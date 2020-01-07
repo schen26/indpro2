7e14413bcbb8689a20d4b886d237e504327013f2
schen26 <simon.chen@academytrainee.com>
2020-01-07 18:12:04 +0000

    service 2 working

--- flask-service-2/application/routes.py
+++ flask-service-2/application/routes.py
@@ -1,12 +1,13 @@
+from application import app
 import string
 import random
 
 @app.route('/')
 def randLet():
-    return random.choice(string.ascii_lowercase))
+    return random.choice(string.ascii_lowercase)
 
-@app.route('/')
-def randLet():
-    return random.choice(string.ascii_uppercase))
+@app.route('/upper')
+def upperLet():
+    return random.choice(string.ascii_uppercase)
 
 #render_template('home.html', random_letter=random.choice(string.ascii_uppercase))
