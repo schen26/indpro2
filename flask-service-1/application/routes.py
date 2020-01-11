42f47808608c6c134d30d795538f0f28f6dde984
schen26 <simon.chen@academytrainee.com>
2020-01-11 20:14:48 +0000

    changed service 1

--- flask-service-1/application/routes.py
+++ flask-service-1/application/routes.py
@@ -11,7 +11,7 @@ from application.models import coordinates
 def hitMiss():
     count=0
     for i in range(25):#while count<25:'''
-    hitMiss=requests.post('http://service4:5003')
+        hitMiss=requests.post('http://service4:5003')
         #random_letter=hitMiss.json()["random_letter"]
         #random_number=hitMiss.json()["random_number"]
         #hitMiss=hitMiss.json()["hitMiss"]
@@ -25,10 +25,10 @@ def hitMiss():
         #db.session.commit()
         return render_template('home.html', random_letter=random_letter, random_number=random_number, hitMiss=hitMiss)
 '''        
-    if prevCoord in coordData:
-        continue
-    else:
-        break
+        if prevCoord in coordData:
+            continue
+        else:
+            break
     prevCoord=coordinates(previous_coordinates=prevCoord)
     db.session.add(prevCoord)
     db.session.commit()
